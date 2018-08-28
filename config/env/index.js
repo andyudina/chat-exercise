a4c823eadf1266aa0a6e413f3e8746476c4c7fe0
Anastasia <anastasia.diudina@gmail.com>
2018-08-28 21:52:39 +0300

    Add production config

--- config/env/index.js
+++ config/env/index.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const development = require('./development'),
+  production = require('./production'),
   test = require('./test');
 
 module.exports = {
   development: development,
   test: test,
+  production: production
 }[process.env.NODE_ENV || 'development'];
---
+++ config/env/production.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+  db: process.env.MONGOHQ_URL,
+  google: {
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    // TODO: change domain after deployed
+    callbackURL: 'http://localhost:3000/auth/google/callback/'
+  },
+  sessions: {
+    secret: process.env.SESSION_SECRET
+  }
+};
