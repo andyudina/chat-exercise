5fce44d38d1b2eba08dd84daa542788d86b5c2e9
Anastasia <anastasia.diudina@gmail.com>
2018-08-29 10:33:36 +0300

    Validate chat id param in url using regex

--- api/routes/user.js
+++ api/routes/user.js
@@ -48,39 +48,39 @@ userRouter.route('/self/chats')
 
 */
 
-userRouter.route('/self/chats/:chatId')
+userRouter.route('/self/chats/:chatId([a-f\\d]{24})')
   .get(
     apiRequiresAuthentication,
     authorizeAccessToChat,
     MessageController.getChatWithMessages);
 
-userRouter.route('/self/chats/:chatId/messages')
+userRouter.route('/self/chats/:chatId([a-f\\d]{24})/messages')
   .get(
     apiRequiresAuthentication,
     authorizeAccessToChat,
     MessageController.listMessagesInChat);
 
-userRouter.route('/self/chats/:chatId/messages')
+userRouter.route('/self/chats/:chatId([a-f\\d]{24})/messages')
   .post(
     apiRequiresAuthentication,
+    authorizeAccessToChat,
     // Specify validation
     [
       body('message', 'This field is required').exists(),
     ],
     validateRequest,
-    authorizeAccessToChat,
     MessageController.sendMessage);
 
 // Returns new messages
 // Expects date in get parameters (after wich messages will be returned)
-userRouter.route('/self/chats/:chatId/messages/new')
+userRouter.route('/self/chats/:chatId([a-f\\d]{24})/messages/new')
   .get(
     apiRequiresAuthentication,
+    authorizeAccessToChat,
     [
       query('date', 'This field is required').exists(),
     ],
     validateRequest,
-    authorizeAccessToChat,
     MessageController.listNewMessagesInChat);
 
 module.exports = userRouter;
